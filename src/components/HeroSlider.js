2f2950b0112dcf1efbc78a39e1e37e3ca8a136d1
Irakli Orjonikidze <irakliorjo92@gmail.com>
2023-05-09 11:43:24 +0400

    initial commit

---
+++ src/components/HeroSlider.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+//import swiper react components
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+
+import {EffectFade, Autoplay} from 'swiper'
+
+import Img1 from '../assets/img/heroSlider/1.jpg'
+import Img2 from '../assets/img/heroSlider/2.jpg'
+import Img3 from '../assets/img/heroSlider/3.jpg'
+
+const slides = [
+  {
+    title: 'Your Luxury Hotel form Vacation',
+    bg: Img1,
+    btnText: 'Room & Suites',
+  },
+  {
+    title: 'Your Luxury Hotel form Vacation',
+    bg: Img2,
+    btnText: 'Room & Suites',
+  },
+  {
+    title: 'Your Luxury Hotel form Vacation',
+    bg: Img3,
+    btnText: 'Room & Suites',
+  },
+]
+
+const HeroSlider = () => {
+  return (
+   <Swiper modules={[EffectFade, Autoplay]} 
+   effect={'fade'}
+   Loop={true}
+   autoplay={{
+    delay: 3000,
+    disableOnInteraction: false,
+   }}
+   className='heroSlider h-[600px] lg:h-[860px]'>
+    {slides.map((slide,index) => {
+      //desctucture slide
+      const {title, bg, btnText} = slide
+      return (<SwiperSlide key={index} className='h-full relative flex justify-center items-center'>
+        <div className='z-20 text-white text-center'>
+          <div className='uppercase font-tertiary tracking-[6px] mb-5 '>
+            Just Enjoy and relax
+          </div>
+          <h1 className='text-[32px] font-primary uppercase tracking-[2px] max-w-[920px] lg:text-[68px] leading-tight mb-6'>
+            {title}
+          </h1>
+          <button className='btn btn-lg btn-primary mx-auto'>
+            {btnText}
+          </button>
+        </div>
+        <div className='absolute top-0 w-full h-full'>
+          <img className='object-cover h-full w-full' src={bg} />
+        </div>
+        <div className='absolute w-full h-full bg-black/70'></div>
+        </SwiperSlide>)
+    })}
+   </Swiper>
+  )
+}
+
+export default HeroSlider
