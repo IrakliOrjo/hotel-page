7a6fa33070ee3a8ca210e4d57ae370844f820462
Irakli Orjonikidze <96581851+IrakliOrjo@users.noreply.github.com>
2023-07-11 22:41:46 +0400

    Update Rooms.js

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,39 +1,58 @@
-import React, {useContext} from 'react'
+import React from 'react'
 
-import { RoomContext } from '../context/RoomContext'
+import {Link} from 'react-router-dom'
 
-import {SpinnerDotted} from 'spinners-react'
+import {BsArrowsFullscreen, BsPeople} from 'react-icons/bs'
 
-import Room from '../components/Room'
 
-const Rooms = () => {
-  const { rooms, loading } = useContext(RoomContext)
+
+const Room = ({room}) => {
+
+  const {id, name, image, size, maxPerson, description, price} = room;
 
   return (
-    <section className='py-24'>
-      {loading && (
-        <div className='h-screen fixed bottom-0 top-0 bg-black/90 w-full
-        z-50 flex justify-center items-center'>
-          <SpinnerDotted color='white' />
-        </div>
-      )}
-      <div className='container mx-auto lg:px-0'>
-        <div className='text-center'>
-          <div className='font-tertiery uppercase text-[15px] tracking-[6px]'>Hotel & Spa Adina</div>
-          <h2 className='font-primary text-[45px] mb-4'>Room & Suites</h2>
-        </div>
-        <div 
-        className='grid grid-cols-1 max-w-sm 
-        mx-auto gap-[30px] lg:grid-cols-3 
-        lg:max-w-none lg:mx-0'>
-          {rooms.map(room => {
-       
-            return <Room room={room} key={room.id} />;
-          })}
+    <div className='bg-white shadow-2xl min-h-[500px] group'>
+      <div className='overflow-hidden'>
+        <img 
+        className='group-hover:scale-110 transition-all duration-300 w-full' src={image} alt='' />
+      </div>
+      <div className='bg-white shadow-lg max-w-[300px] mx-auto h-[60px]
+      -translate-y-1/2 flex justify-center items-center uppercase font-tertiary tracking-[1px] font-semibold text-base'>
+        <div className='flex justify-between w-[80%]'>
+          <div className='flex items-center gap-x-2'>
+            <div className='text-accent'>
+              <BsArrowsFullscreen className='text-[15px]' />
+            </div>
+            <div className='flex gap-x-1'>
+              <div className='text-[13px] font-secondary'>Size</div>
+              <div className='text-[13px] font-secondary'>{size}m2</div>
+            </div>
+          </div>
+          <div className='flex items-center gap-x-2'>
+            <div className='text-accent'>
+              <BsPeople className='text-[18px]' />
+            </div>
+            <div className='flex gap-x-1'>
+              <div className='text-[13px] font-secondary'>Max People</div>
+              <div className='text-[13px] font-secondary'>{maxPerson}</div>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+      {/* name */}
+      <div className='text-center '>
+        <Link to={`/room/${id}`}>
+          <h3 className='h3'>{name}</h3>
+        </Link>
+        <p className='max-w-[300px] mx-auto mb-3 lg:mb-6'>{description.slice(0,56)}</p>
+      </div>
+      <Link to={`/room/${id}`}
+       className='btn btn-secondary btn-sm max-w-[240px] mx-auto'
+      >
+        Book now from ${price}
+      </Link>
+    </div>
   )
 }
 
-export default Rooms
+export default Room
