f9a4f3ce40248a9dd62fa574a2eda4179d21f8ba
Irakli Orjonikidze <96581851+IrakliOrjo@users.noreply.github.com>
2023-07-11 22:41:58 +0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,10 @@ import LogoWhite from '../assets/img/logo-white.svg'
 const Footer = () => {
   return (
     <footer className='bg-primary py-12 '>
-      <div className='container mx-auto text-white flex justify-between'>
+      <div className='container mx-auto text-white flex flex-col
+       md:flex-row justify-center items-center md:justify-start md:items-start gap-3 md:gap-0'>
         <a href='/' >
-        <img src={LogoWhite} />
+        <img className='lg:mr-[18.5em] md:mr-[10em]' src={LogoWhite} />
         </a>
         Copyright &copy; 2023. All rights reserved.
       </div>
